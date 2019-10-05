26ca7cd675851db0deea849f54a580dc134992f4
Mathias Hansen <me@codemonkey.io>
2019-10-04 20:34:22 -0400

    Changed maps.geocod.io to tile-cdn.geocod.io

--- resources/views/partials/result.blade.php
+++ resources/views/partials/result.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-{{ $result->accuracy >= 0.7 ? 'green': 'orange' }}-100 border-l-4 border-{{ $result->accuracy >= 0.7 ? 'green': 'orange' }}-500 text-{{ $result->accuracy >= 0.7 ? 'green': 'orange' }}-700 mb-4">
 
-    <iframe src="https://maps.geocod.io/#{{ $result->location->lat }},{{ $result->location->lng }}" class="w-full" height="200"></iframe>
+    <iframe src="https://tile-cdn.geocod.io/#{{ $result->location->lat }},{{ $result->location->lng }}" class="w-full" height="200"></iframe>
 
     <div class="p-4">
         <p class="mb-4 text-xl font-mono">{{ $result->formatted_address ?? 'Unknown' }}</p>
